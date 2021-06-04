d7be225052c6cee45421e4d07b50d1da738aca68
Alessia Amitrano <alessia@phntms.com>
2021-06-04 09:11:54 +0100

    tweaks on accessibility

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -12,7 +12,11 @@ function GameCard({ game, handleToggleLightbox }) {
 
   return (
     <div className={styles.card} style={gridArea} onClick={handleGameClick}>
-      <img src={`assets/${game.artwork}`} alt={game.alt_text} />
+      <img
+        src={`assets/${game.artwork}`}
+        alt={game.alt_text}
+        aria-hidden="true"
+      />
       <div className={styles.cardText}>
         <h2>{game.title}</h2>
         <p>{game.content.substring(0, isMobile ? 50 : 100) + "..."}</p>
